348f36a3868d5734ff8c9a222f95cd6812073ede
andersonmerten <anderson.merten@gmail.com>
2018-05-03 20:59:54 -0300

    add sales

--- CRUD/src/main/java/com/aim/recanto/CRUD/controller/PostController.java
+++ CRUD/src/main/java/com/aim/recanto/CRUD/controller/PostController.java
@@ -7,11 +7,13 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import com.aim.recanto.CRUD.model.Post;
 import com.aim.recanto.CRUD.service.PostService;
  
 @Controller
+@RequestMapping("post")
 public class PostController {
      
     @Autowired
@@ -48,7 +50,7 @@ public class PostController {
          
         return findAll();
     }
- 
+    
     @PostMapping("/save")
     public ModelAndView save(@Valid Post post, BindingResult result) {
          
--- CRUD/src/main/java/com/aim/recanto/CRUD/controller/VendaController.java
+++ CRUD/src/main/java/com/aim/recanto/CRUD/controller/VendaController.java
@@ -8,12 +8,14 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.aim.recanto.CRUD.model.Venda;
 import com.aim.recanto.CRUD.service.VendaService;
 
 @Controller
+@RequestMapping("venda")
 public class VendaController {
 	
 	@Autowired
